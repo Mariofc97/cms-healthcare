1e5b164d075e2927c7217ae2d4a3db4fe8714d34
Felipe Ferreira <fefucho08@gmail.com>
2025-04-08 22:48:12 -0700

    Add access roles for staff and diagnoses routes

--- routes/diagnoses.php
+++ routes/diagnoses.php
@@ -4,6 +4,7 @@ ini_set('display_errors', 1);
 use audit\AuditGenerator; //Import AuditGenerator class from the 'audit' namespace(Audit.php)
 use audit\Outcome;  // Import Outcome enum(enumeration) from the 'audit' namespace
 use models\Diagnosis;  //Import Diagnosis class from the 'models' namespace(Content.php)
+use models\User;
 
 require_once __DIR__ . '/../models/User.php';  // Load User.php using an absolute path based on the current directory
 require_once __DIR__ . '/../models/Content.php';
@@ -13,48 +14,56 @@ require_once __DIR__ . '/../models/Audit.php';
 $method = $_SERVER["REQUEST_METHOD"];
 switch ($method) {
   case "GET":  //get diagnoses
-    $id = $_GET["diagnosisID"] ?? null; // Get the 'diagnosisID' parameter from the URL and assign it to $id
-    if (!isset($id)) { // Check if the 'id' variable is set
-      throw new Exception("Parameters missing", 400); //400 Bad Request, client error response status code
-    }
-    if (empty($id)) {
-      throw new Exception("Parameters cannnot be empty", 400);
-    }
-    $id = filter_var($_GET["diagnosisID"]) ?? null; // Get the 'diagnosisID' parameter from the URL and assign it to $id
+    if ($_SESSION["userInfo"]["Type"] === User::DOCTOR || $_SESSION["userInfo"]["Type"] === User::PATIENT) {
+      $id = $_GET["diagnosisID"] ?? null; // Get the 'diagnosisID' parameter from the URL and assign it to $id
+      if (!isset($id)) { // Check if the 'id' variable is set
+        throw new Exception("Parameters missing", 400); //400 Bad Request, client error response status code
+      }
+      if (empty($id)) {
+        throw new Exception("Parameters cannnot be empty", 400);
+      }
+      $id = filter_var($_GET["diagnosisID"]) ?? null; // Get the 'diagnosisID' parameter from the URL and assign it to $id
 
-    $controller = new DiagnosisController(); //creat new instance (contentController.php)
-    try {
-      $diagnosis = $controller->getById($id); //call getById function with $id to get diagnosis info matching the diagnosesID
-      $prescriptionController = new PrescriptionController(); //creat new instance (contentController.php)
-      $diagnosis->setPrescriptions($prescriptionController->getByDiagnosis($diagnosis->getId()));
-      // Fetch and set prescriptions for the diagnosis based on the diagnosis' ID
-      AuditGenerator::genarateLog("root", "Get Diagnoses", Outcome::SUCCESS);
-      echo json_encode($diagnosis);
-    } catch (Exception $e) {
-      AuditGenerator::genarateLog("root", "Get Diagnoses", Outcome::ERROR);
-      throw new Exception("Error getting diagnosis." . $e->getMessage(), 500); //500 Internal Server Error, server error response status code
+      $controller = new DiagnosisController(); //creat new instance (contentController.php)
+      try {
+        $diagnosis = $controller->getById($id); //call getById function with $id to get diagnosis info matching the diagnosesID
+        $prescriptionController = new PrescriptionController(); //creat new instance (contentController.php)
+        $diagnosis->setPrescriptions($prescriptionController->getByDiagnosis($diagnosis->getId()));
+        // Fetch and set prescriptions for the diagnosis based on the diagnosis' ID
+        AuditGenerator::genarateLog("root", "Get Diagnoses", Outcome::SUCCESS);
+        echo json_encode($diagnosis);
+      } catch (Exception $e) {
+        AuditGenerator::genarateLog("root", "Get Diagnoses", Outcome::ERROR);
+        throw new Exception("Error getting diagnosis." . $e->getMessage(), 500); //500 Internal Server Error, server error response status code
+      }
+    } else {
+      throw new Exception("User not allowed", 403);
     }
     break;
   case "POST": //adding new diagnosis
-    $appointmentid = $_POST["appointmentID"] ?? null;  // Get the 'appointmentID' parameter from the URL and assign it to $appointmentid
-    $description = $_POST["description"] ?? null;  // Get the 'description' parameter from the URL and assign it to $description
-    if (!isset($appointmentid) || !isset($description)) {
-      throw new Exception("Parameters missing", 400);
-    }
-    if (empty($appointmentid) || empty($description)) {
-      throw new Exception("Parameters cannnot be empty", 400);
-    }
-    $appointmentid = filter_var($_POST["appointmentID"], FILTER_SANITIZE_NUMBER_INT) ?? null;  // Get the 'appointmentID' parameter from the URL and assign it to $appointmentid
-    $description = htmlspecialchars(strip_tags($_POST["description"])) ?? null;  // Get the 'description' parameter from the URL and assign it to $description
+    if ($_SESSION["userInfo"]["Type"] === User::DOCTOR) {
+      $appointmentid = $_POST["appointmentID"] ?? null;  // Get the 'appointmentID' parameter from the URL and assign it to $appointmentid
+      $description = $_POST["description"] ?? null;  // Get the 'description' parameter from the URL and assign it to $description
+      if (!isset($appointmentid) || !isset($description)) {
+        throw new Exception("Parameters missing", 400);
+      }
+      if (empty($appointmentid) || empty($description)) {
+        throw new Exception("Parameters cannnot be empty", 400);
+      }
+      $appointmentid = filter_var($_POST["appointmentID"], FILTER_SANITIZE_NUMBER_INT) ?? null;  // Get the 'appointmentID' parameter from the URL and assign it to $appointmentid
+      $description = htmlspecialchars(strip_tags($_POST["description"])) ?? null;  // Get the 'description' parameter from the URL and assign it to $description
 
-    try {
-      $newDiagnosis = new Diagnosis(0, $description, $appointmentid); // Create a new Diagnosis instance with the given values (Content.php)
-      $controller = new DiagnosisController();
-      $controller->newRecord($newDiagnosis);
-      AuditGenerator::genarateLog("root", "Adding Diagnosis", outcome::SUCCESS);
-    } catch (Exception $e) {
-      AuditGenerator::genarateLog("root", "Adding Diagnosis", outcome::ERROR);
-      throw new Exception("Error adding diagnosis" . $e->getMessage(), 500);
+      try {
+        $newDiagnosis = new Diagnosis(0, $description, $appointmentid); // Create a new Diagnosis instance with the given values (Content.php)
+        $controller = new DiagnosisController();
+        $controller->newRecord($newDiagnosis);
+        AuditGenerator::genarateLog("root", "Adding Diagnosis", outcome::SUCCESS);
+      } catch (Exception $e) {
+        AuditGenerator::genarateLog("root", "Adding Diagnosis", outcome::ERROR);
+        throw new Exception("Error adding diagnosis" . $e->getMessage(), 500);
+      }
+    } else {
+      throw new Exception("User not allowed", 403);
     }
     break;
   default:
--- routes/staff.php
+++ routes/staff.php
@@ -14,91 +14,94 @@ $method = $_SERVER["REQUEST_METHOD"];
 $requestUri = trim($_SERVER["PATH_INFO"], "/") ?? "";
 //If PATH_INFO exists, retrieve the string with the leading and trailing slashes removed; otherwise, use an empty string
 $path = explode("/", $requestUri);
+if ($_SESSION["userInfo"]["Type"] === User::STAFF) {
+  switch ($method) {
+    case "POST":
+      $fname = $_POST["fname"] ?? null; // Get the 'fname' parameter from the URL and assign it to $fname
+      $lname = $_POST["lname"] ?? null;
+      $phone = $_POST["phone"] ?? null;
+      $email = $_POST["email"] ?? null;
+      $password = $_POST["pass"] ?? null;
 
-switch ($method) {
-  case "POST":
-    $fname = $_POST["fname"] ?? null; // Get the 'fname' parameter from the URL and assign it to $fname
-    $lname = $_POST["lname"] ?? null;
-    $phone = $_POST["phone"] ?? null;
-    $email = $_POST["email"] ?? null;
-    $password = $_POST["pass"] ?? null;
-
-    //update staff member's info, delete staff
-    if (in_array("update", $path) || in_array("delete", $path)) {
-      $id = $_POST["id"] ?? null;
-      if (!isset($id) || empty($id)) {
-        throw new Exception("User ID is missing", 400);
-      }
-      $id = filter_var($_POST["id"]) ?? null;
-
-      $controller = new StaffController();
-      if (in_array("update", $path)) {  //update staff info
-        try {
-          $updateStaff = $controller->getById($id); //contentController.php > StaffControlloer
-          if (isset($fname) && !empty($fname)) {
-            $fname = htmlspecialchars(strip_tags($_POST["fname"])) ?? null; // Sanitize and assign it to $fname
-            $updateStaff->setFname($fname);  //User.php > class Staff
-          }
-          if (isset($lname) && !empty($lname)) {
-            $lname = htmlspecialchars(strip_tags($_POST["lname"])) ?? null;
-            $updateStaff->setLname($lname);
-          }
-          if (isset($phone) && !empty($phone)) {
-            $phone = htmlspecialchars(strip_tags($_POST["phone"])) ?? null;
-            $updateStaff->setPhone($phone);
-          }
-          if (isset($email) && !empty($email)) {
-            $email = htmlspecialchars(strip_tags($_POST["email"])) ?? null;
-            $updateStaff->setEmail($email);
-          }
-          if (isset($password) && !empty($password)) {
-            $password = password_hash(strip_tags($_POST["pass"]), PASSWORD_DEFAULT, ['cost' => 10]) ?? null; //Sanitize and convert the password into a hashed value
-            $updateStaff->setPassword($password);
-          }
-
-          $controller->updateRecord($updateStaff);
-          AuditGenerator::genarateLog("root", "Update Staff", Outcome::SUCCESS);
-          echo json_encode("Staff updated successfully");
-        } catch (Exception $e) {
-          AuditGenerator::genarateLog("root", "Update Staff", Outcome::ERROR);
-          throw new Exception("Error updating staff information. " . $e->getMessage(), 500); //500 Internal Server Error, server error response status code
+      //update staff member's info, delete staff
+      if (in_array("update", $path) || in_array("delete", $path)) {
+        $id = $_POST["id"] ?? null;
+        if (!isset($id) || empty($id)) {
+          throw new Exception("User ID is missing", 400);
         }
-      } elseif (in_array("delete", $path)) {  //delete(deactivate) staff
-        try {
-          $controller->deleteStaff($id);
-          AuditGenerator::genarateLog("root", "Delete Staff", Outcome::SUCCESS);
-          echo json_encode("Staff deleted successfully");
-        } catch (Exception $e) {
-          AuditGenerator::genarateLog("root", "Delete Staff", Outcome::ERROR);
-          throw new Exception("Error deleting staff information. " . $e->getMessage(), 500);
-        }
-      }
+        $id = filter_var($_POST["id"]) ?? null;
 
-      //add new staff member's to the system
-    } else {
-      if (!isset($fname) || !isset($lname) || !isset($phone) || !isset($email) || !isset($password)) {
-        throw new Exception("Parameters missing", 400); //400 Bad Request, client error response status code
-      }
-      if (empty($fname) || empty($lname) || empty($phone) || empty($email) || empty($password)) {
-        throw new Exception("Parameters cannnot be empty", 400);
-      }
-      $fname = htmlspecialchars(strip_tags($_POST["fname"])) ?? null; // Sanitize and assign it to $fname
-      $lname = htmlspecialchars(strip_tags($_POST["lname"])) ?? null;
-      $phone = htmlspecialchars(strip_tags($_POST["phone"])) ?? null;
-      $email = htmlspecialchars(strip_tags($_POST["email"])) ?? null;
-      $password = password_hash(strip_tags($_POST["pass"]), PASSWORD_DEFAULT, ['cost' => 10]) ?? null; //Sanitize and convert the password into a hashed value
-
-      $newStaff = new Staff(0, $fname, $lname, $phone, $email, $password, User::STAFF);
-      try {
         $controller = new StaffController();
-        $controller->newRecord($newStaff);
-        AuditGenerator::genarateLog("root", "Add New Staff", outcome::SUCCESS);
-      } catch (Exception $e) {
-        AuditGenerator::genarateLog("root", "Add New Staff", outcome::ERROR);
-        throw new Exception("Error adding new staff. " . $e->getMessage(), 500);
+        if (in_array("update", $path)) {  //update staff info
+          try {
+            $updateStaff = $controller->getById($id); //contentController.php > StaffControlloer
+            if (isset($fname) && !empty($fname)) {
+              $fname = htmlspecialchars(strip_tags($_POST["fname"])) ?? null; // Sanitize and assign it to $fname
+              $updateStaff->setFname($fname);  //User.php > class Staff
+            }
+            if (isset($lname) && !empty($lname)) {
+              $lname = htmlspecialchars(strip_tags($_POST["lname"])) ?? null;
+              $updateStaff->setLname($lname);
+            }
+            if (isset($phone) && !empty($phone)) {
+              $phone = htmlspecialchars(strip_tags($_POST["phone"])) ?? null;
+              $updateStaff->setPhone($phone);
+            }
+            if (isset($email) && !empty($email)) {
+              $email = htmlspecialchars(strip_tags($_POST["email"])) ?? null;
+              $updateStaff->setEmail($email);
+            }
+            if (isset($password) && !empty($password)) {
+              $password = password_hash(strip_tags($_POST["pass"]), PASSWORD_DEFAULT, ['cost' => 10]) ?? null; //Sanitize and convert the password into a hashed value
+              $updateStaff->setPassword($password);
+            }
+
+            $controller->updateRecord($updateStaff);
+            AuditGenerator::genarateLog("root", "Update Staff", Outcome::SUCCESS);
+            echo json_encode("Staff updated successfully");
+          } catch (Exception $e) {
+            AuditGenerator::genarateLog("root", "Update Staff", Outcome::ERROR);
+            throw new Exception("Error updating staff information. " . $e->getMessage(), 500); //500 Internal Server Error, server error response status code
+          }
+        } elseif (in_array("delete", $path)) {  //delete(deactivate) staff
+          try {
+            $controller->deleteStaff($id);
+            AuditGenerator::genarateLog("root", "Delete Staff", Outcome::SUCCESS);
+            echo json_encode("Staff deleted successfully");
+          } catch (Exception $e) {
+            AuditGenerator::genarateLog("root", "Delete Staff", Outcome::ERROR);
+            throw new Exception("Error deleting staff information. " . $e->getMessage(), 500);
+          }
+        }
+
+        //add new staff member's to the system
+      } else {
+        if (!isset($fname) || !isset($lname) || !isset($phone) || !isset($email) || !isset($password)) {
+          throw new Exception("Parameters missing", 400); //400 Bad Request, client error response status code
+        }
+        if (empty($fname) || empty($lname) || empty($phone) || empty($email) || empty($password)) {
+          throw new Exception("Parameters cannnot be empty", 400);
+        }
+        $fname = htmlspecialchars(strip_tags($_POST["fname"])) ?? null; // Sanitize and assign it to $fname
+        $lname = htmlspecialchars(strip_tags($_POST["lname"])) ?? null;
+        $phone = htmlspecialchars(strip_tags($_POST["phone"])) ?? null;
+        $email = htmlspecialchars(strip_tags($_POST["email"])) ?? null;
+        $password = password_hash(strip_tags($_POST["pass"]), PASSWORD_DEFAULT, ['cost' => 10]) ?? null; //Sanitize and convert the password into a hashed value
+
+        $newStaff = new Staff(0, $fname, $lname, $phone, $email, $password, User::STAFF);
+        try {
+          $controller = new StaffController();
+          $controller->newRecord($newStaff);
+          AuditGenerator::genarateLog("root", "Add New Staff", outcome::SUCCESS);
+        } catch (Exception $e) {
+          AuditGenerator::genarateLog("root", "Add New Staff", outcome::ERROR);
+          throw new Exception("Error adding new staff. " . $e->getMessage(), 500);
+        }
       }
-    }
-    break;
-  default:
-    throw new Exception("$method reqest method is not allowed.", 405); //405 Method Not Allowed, client error response status code
+      break;
+    default:
+      throw new Exception("$method reqest method is not allowed.", 405); //405 Method Not Allowed, client error response status code
+  }
+} else {
+  throw new Exception("User not allowed", 403);
 }
